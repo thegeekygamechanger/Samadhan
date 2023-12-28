bfac4b9552bfe3508cc53ac67a7e8f2329eab490
Harsh kumar <97349372+thegeekygamechanger@users.noreply.github.com>
2023-12-28 15:23:22 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+// App.js
+
+import React from 'react';
+import MyComponent from './MyComponent';
+import ContactUs from './ContactUs';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <MyComponent />
+      {/* Other components or content */}
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
