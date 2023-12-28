25981518302d8e262b952fe6a03e4401bb9e90ed
Harsh kumar <97349372+thegeekygamechanger@users.noreply.github.com>
2023-12-28 15:24:20 +0530

    Create MyComponent.js

---
+++ MyComponent.js
@@ -0,0 +1,14 @@
+// MyComponent.js
+
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is my first React component.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
