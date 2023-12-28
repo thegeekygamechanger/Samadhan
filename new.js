c9f40866d0daa1052515ae093d8f023596248676
Harsh kumar <97349372+thegeekygamechanger@users.noreply.github.com>
2023-12-28 15:24:49 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+
+function ContactUs() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>If you have any questions or concerns, please feel free to reach out to us:</p>
+      <ul>
+        <li>Email: contact@example.com</li>
+        <li>Phone: (123) 456-7890</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ContactUs;
